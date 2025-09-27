a6c181171fbe016624c4b1152dc19d45fd64471c
= <=>
2025-09-26 22:14:44 -0300

    Try catch add

--- ex01-express/api/routes/message.js
+++ ex01-express/api/routes/message.js
@@ -5,86 +5,122 @@ const router = Router();
 const messagesModel = models.Message;
 
 router.get("/", async (req, res) => {
-  const getMessages = await messagesModel.findAll();
+  try {
+    const getMessages = await messagesModel.findAll();
 
-  return res.status(200).send({
-    message : "Exibindo todas as mensagens",
-    data : getMessages
-  });
+    return res.status(200).send({
+      message : "Exibindo todas as mensagens",
+      data : getMessages
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
+  }
 });
 
 router.get("/:messageId", async (req, res) => {
-  const { messageId } = req.params;
+  try {
+    const { messageId } = req.params;
 
-  const getMessage = await messagesModel.findByPk(messageId);
+    const getMessage = await messagesModel.findByPk(messageId);
 
-  if(!getMessage){
-    return res.status(404).send({
-      error : "Mensagem não encontrada"
-    })
+    if(!getMessage){
+      return res.status(404).send({
+        error : "Mensagem não encontrada"
+      })
+    }
+
+    return res.status(200).send({
+      message : "Mensagem encontrada",
+      data : getMessage,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
   }
 
-  return res.status(200).send({
-    message : "Mensagem encontrada",
-    data : getMessage,
-  });
 });
 
 router.post("/", async (req, res) => {
-  const { text } = req.body
+  try {
+    const { text } = req.body
 
-  const message = {
-    text: text,
-    userId: req.context.me.id,
-  };
+    const message = {
+      text: text,
+      userId: req.context.me.id,
+    };
 
-  if(!text || !req.context.me?.id){
-    return res.status(400).send({error : "Por favor preencha os campos ou efetue o seu login!!"})
+    if(!text || !req.context.me?.id){
+      return res.status(400).send({error : "Por favor preencha os campos ou efetue o seu login!!"})
+    }
+
+    const createdMessage = await messagesModel.create(message);
+
+    return res.status(201).send({
+      message : "Mensagem enviada",
+      data : createdMessage
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
   }
-
-  const createdMessage = await messagesModel.create(message);
-
-  return res.status(201).send({
-    message : "Mensagem enviada",
-    data : createdMessage
-  });
 });
 
 router.put("/:messageId", async (req, res) => {
-  const { messageId } = req.params;
-  const { text } = req.body;
+  try {
+    const { messageId } = req.params;
+    const { text } = req.body;
 
-  const getMessage = await messagesModel.findByPk(messageId);
-  if(!getMessage){
-    return res.status(404).send({
-      error : "Mensagem não encontrada"
-    })}
+    const getMessage = await messagesModel.findByPk(messageId);
+    if(!getMessage){
+      return res.status(404).send({
+        error : "Mensagem não encontrada"
+      })}
 
-  await messagesModel.update({text}, {where: {
-    id : messageId
-  }})
+    await messagesModel.update({text}, {where: {
+      id : messageId
+    }})
 
-  return res.status(200).send({
-    message : "A mensagem foi atualizada"
-  })
+    return res.status(200).send({
+      message : "A mensagem foi atualizada"
+    })
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
+  }
 })
 
 router.delete("/:messageId", async (req, res) => {
-  const { messageId } = req.params;
+  try {
+    const { messageId } = req.params;
 
-  const getMessage = await messagesModel.findByPk(messageId);
+    const getMessage = await messagesModel.findByPk(messageId);
 
-  if(!getMessage){
-    return res.status(404).send({
-      error : "Mensagem não encontrada"
-    })
+    if(!getMessage){
+      return res.status(404).send({
+        error : "Mensagem não encontrada"
+      })
+    }
+
+    await messagesModel.destroy({where : {id : messageId}});
+
+    return res.status(200).send({
+      message : "Mensagem deletada"
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
   }
-
-  await messagesModel.destroy({where : {id : messageId}});
-
-  return res.status(200).send({
-    message : "Mensagem deletada"
-  });
 });
 
 export default router;
--- ex01-express/api/routes/user.js
+++ ex01-express/api/routes/user.js
@@ -5,100 +5,134 @@ const router = Router();
 const users = models.User;
 
 router.get("/", async (req, res) => {
-  const allUsers = await users.findAll();
+  try {
+    const allUsers = await users.findAll();
 
-  return res.status(200).send({
-    "data" : allUsers,
-  });
+    return res.status(200).send({
+      "data" : allUsers,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
+  }
 });
 
 router.get("/:userId", async (req, res) => {
-  const { userId } = req.params;
+  try {
+    const { userId } = req.params;
 
+    const getUser = await users.findByPk(userId);
 
-  const getUser = await users.findByPk(userId);
-
-  if(!getUser){
-    return res.status(404).send({
-      error: "Usuário não encointrado",
+    if(!getUser){
+      return res.status(404).send({
+        error: "Usuário não encointrado",
+      });
+    }
+    return res.status(200).send({
+      message : "Usuário encontrado",
+      data : getUser
     });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
   }
-  return res.status(200).send({
-    message : "Usuário encontrado",
-    data : getUser
-  });
 });
 
 router.post("/", async (req, res) => {
-  const { username,email } = req.body;
+  try {
+    const { username,email } = req.body;
 
-  if(!username || !email){
-    return res.status(400).send({
-      error : "Preencha os campos obrigatórios!!"
-    })
+    if(!username || !email){
+      return res.status(400).send({
+        error : "Preencha os campos obrigatórios!!"
+      })
+    }
+    
+    const newUserData = {
+      email,
+      username 
+    }
+
+    const newUser = await users.create(newUserData);
+    
+    return res.status(201).send({
+      message : "Usário criado",
+      data : newUser
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
   }
   
-  const newUserData = {
-    email,
-    username 
-  }
-
-  const newUser = await users.create(newUserData);
-  
-  return res.status(201).send({
-    message : "Usário criado",
-    data : newUser
-  });
 });
 
 router.put("/:userId", async(req, res) => {
-  const { userId } = req.params
-  const { username,email } = req.body;
+  try {
+    const { userId } = req.params
+    const { username,email } = req.body;
 
-  const userExist = await users.findByPk(userId);
+    const userExist = await users.findByPk(userId);
 
-  if(!userExist){
-    return res.status(404).send({
-      error: "Usuário não encontrado!!"
-    })
+    if(!userExist){
+      return res.status(404).send({
+        error: "Usuário não encontrado!!"
+      })
+    }
+
+    const newUserData = {
+      email : email,
+      username: username 
+    }
+
+    await users.update(newUserData, {where: {
+      id : userId
+    }})
+
+    return res.status(200).send({
+      message : "Usuário atualizado",
+      data : newUserData,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
   }
-
-  const newUserData = {
-    email : email,
-    username: username 
-  }
-
-  await users.update(newUserData, {where: {
-    id : userId
-  }})
-
-  return res.status(200).send({
-    message : "Usuário atualizado",
-    data : newUserData,
-  });
+  
 });
 
 router.delete("/:userId", async (req, res) => {
-  const { userId } = req.params;
+  try {
+    const { userId } = req.params;
 
-  const userExist = await users.findByPk(userId);
+    const userExist = await users.findByPk(userId);
 
-  if(!userExist){
-    return res.status(404).send({
-      error: "Usuário não encontrado!!"
-    })
-  }
-
-
-  await users.destroy({
-    where:{
-      id : userId,
+    if(!userExist){
+      return res.status(404).send({
+        error: "Usuário não encontrado!!"
+      })
     }
-  })
 
-  return res.status(204).send({
-    message : "Usuário deletado com sucesso!!"
-  });
+    await users.destroy({
+      where:{
+        id : userId,
+      }
+    })
+
+    return res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({
+      error : "Erro interno do servidor."
+    }); 
+  }
+  
 });
 
 export default router;
